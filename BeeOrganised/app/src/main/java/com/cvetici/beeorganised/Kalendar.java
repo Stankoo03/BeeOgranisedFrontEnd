85f4fb6af36327e5f728f6760648c4caf226ccc1
Aleksa Stankovic <aleksastbusiness@gmail.com>
2022-01-11 21:25:23 +0100

    Dodato dodavanje taskova preko kalendara

--- BeeOrganised/app/src/main/java/com/cvetici/beeorganised/Kalendar.java
+++ BeeOrganised/app/src/main/java/com/cvetici/beeorganised/Kalendar.java
@@ -2,31 +2,43 @@ package com.cvetici.beeorganised;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Array;
+import java.lang.reflect.Type;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
-public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnItemListener  {
+public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnItemListener, TimePickerDialog.OnTimeSetListener  {
 
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
@@ -38,6 +50,17 @@ public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnIte
     private View bottomSheetView;
     private RadioGroup RG;
 
+    private EditText enterTask;
+    private Button CalculateBtn,SetBtn;
+    private LinearLayout ManualTimeLayout,AiLayout,AfterCalculateBtn;
+    private Spinner prioritySp,timeSp,durationSp;
+
+    private Button FromTime,ToTime;
+    private int h1=-1,h2=-1,m1=-1,m2=-1;
+    private SmartToDo std;
+
+    private List<Task> currentList;
+
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     //istrazi sta je ovo Requires API
@@ -47,6 +70,13 @@ public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnIte
         setContentView(R.layout.activity_kalendar);
         initWigets();
         selectDate = LocalDate.now();
+        FindViews();
+        setMonthView();
+        RadioGroupClicked();
+        FromToTimeSetter();
+
+    }
+    private void FindViews(){
         task = (ExtendedFloatingActionButton)findViewById(R.id.SimpleButtonKalendar);
         fromButton = AnimationUtils.loadAnimation(this,R.anim.from_bottom_anim);
         toButton = AnimationUtils.loadAnimation(this,R.anim.to_bottom_anim);
@@ -59,10 +89,27 @@ public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnIte
                 R.layout.layout_bottom_sheet,(LinearLayout)findViewById(R.id.bottomSheetContainer)
         );
         bottomSheetDialog.setContentView(bottomSheetView);
+
+        RG = bottomSheetView.findViewById(R.id.RadioGroup);
+        ManualTimeLayout = bottomSheetView.findViewById(R.id.ManualTimeLayout);
+        AiLayout = bottomSheetView.findViewById(R.id.AiTimeLayout);
+        FromTime = bottomSheetView.findViewById(R.id.fromTime);
+        ToTime = bottomSheetView.findViewById(R.id.toTime);
+        AfterCalculateBtn = bottomSheetView.findViewById(R.id.AfterBtnClicked);
+        CalculateBtn = bottomSheetView.findViewById(R.id.CalculateBtn);
+        SetBtn = (Button) bottomSheetView.findViewById(R.id.SetBtn);
+        enterTask = (EditText) bottomSheetView.findViewById(R.id.enterTask);
+        prioritySp = bottomSheetView.findViewById(R.id.prioritySpinner);
+        timeSp = bottomSheetView.findViewById(R.id.whenSpinner);
+        durationSp = bottomSheetView.findViewById(R.id.durationSpinner);
+
+        std = new SmartToDo(5);
         task.shrink();
-        setMonthView();
+
+
 
     }
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void setMonthView(){
         monthYearText.setText(monthYearFromDate(selectDate));
@@ -120,8 +167,10 @@ public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnIte
     @Override
     public void onItemClick(int position, String dayText) {
         if(!dayText.equals(" ")){
-            task.setVisibility(View.VISIBLE);
-            task.startAnimation(fromButton);
+            if(task.getVisibility()!=View.VISIBLE) {
+                task.setVisibility(View.VISIBLE);
+                TaskVisibilityAnimation();
+            }
             if(dayText.startsWith("0")){
                 currentDay= Integer.parseInt(dayText.substring(1,2));
             }else {
@@ -135,16 +184,29 @@ public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnIte
             }else {
                 currentMonth = Integer.parseInt(Mesec);
             }
-            Toast.makeText(this, currentDay+" "+currentMonth+" "+currentYear, Toast.LENGTH_SHORT).show();
+
         }else{
-            task.setVisibility(View.INVISIBLE);
+            if(task.getVisibility()==View.VISIBLE) {
+                task.setVisibility(View.INVISIBLE);
+                TaskVisibilityAnimation();
+            }
+
         }
 
     }
+    private void TaskVisibilityAnimation(){
+        if(task.getVisibility()==View.INVISIBLE){
+            task.startAnimation(toButton);
+        }else{
+            task.startAnimation(fromButton);
+        }
+
+
+    }
+
 
     public void dodajTaskNaKalendar(View view) {
         if(task.isExtended()){
-
             bottomSheetDialog.show();
             task.shrink();
         }else{
@@ -155,7 +217,6 @@ public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnIte
     }
 
     private void RadioGroupClicked(){
-/*
         RG.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
@@ -164,7 +225,7 @@ public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnIte
                         ManualTimeLayout.setVisibility(View.VISIBLE);
                         AiLayout.setVisibility(View.GONE);
                         AfterCalculateBtn.setVisibility(View.GONE);
-                        CaluculateBtn.setVisibility(View.GONE);
+                        CalculateBtn.setVisibility(View.GONE);
                         durationSp.setVisibility(View.GONE);
 
                         break;
@@ -174,15 +235,91 @@ public class Kalendar extends AppCompatActivity implements CalendarAdapter.OnIte
                         }
                         AiLayout.setVisibility(View.VISIBLE);
                         durationSp.setVisibility(View.VISIBLE);
-                        CaluculateBtn.setVisibility(View.VISIBLE);
-
+                        CalculateBtn.setVisibility(View.VISIBLE);
 
                 }
             }
         });
-*/
+
+    }
+    private void FromToTimeSetter(){
+        FromTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker  = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(),"tp1");
+            }
+        });
+        ToTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DialogFragment timePicker  = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(),"tp2");
+            }
+        });
+
     }
 
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        Fragment dateTo =Kalendar.this.getSupportFragmentManager().findFragmentByTag("tp1");
+        if(dateTo!=null) {
+            FromTime.setText("Starting time: " + hourOfDay+ ":" + minute);
+            h1=hourOfDay;
+            m1=minute;
+            dateTo=null;
+
+        }else{
+            h2=hourOfDay;
+            m2=minute;
+            ToTime.setText("Ending Time: "+ hourOfDay+ ":" + minute);
+
+        }
+        ShowSetSimpleTaskBtn();
+
+    }
+
+    private void ShowSetSimpleTaskBtn() {
+        if(h1!=-1 && h2!=-1 && m1!=-1 && m2 !=-1){
+            SetBtn.setVisibility(View.VISIBLE);
+            SetBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Task temp = new Task(enterTask.getText().toString(),new Interval(new DateTime(currentYear,currentMonth,currentDay,h1,m1),new DateTime(currentYear,currentMonth,currentDay,h2,m2)));
+                    std.AddTask(temp);
+                    load(currentDay,currentMonth,currentYear);
+                    //currentList = std.GetTasksInInterval(new Interval(new DateTime(Year,Month,Danas,0,1),new DateTime(Year,Month,Danas,23,59)));
+                    currentList.add(temp);
+                    Toast.makeText(Kalendar.this, "Task uspesno zadat za vreme "+currentDay+"."+currentMonth+"."+currentYear, Toast.LENGTH_SHORT).show();
+                    //TODO Pogledaj ovo andrijo
+                    save(currentDay,currentMonth,currentYear);
+                }
+            });
+        }
+    }
+
+    public void save(int dan, int mesec, int godina){
+        String FILE_NAME=dan+"_"+mesec+"_"+godina;
+        SharedPreferences sharedPreferences = getSharedPreferences(FILE_NAME,MODE_PRIVATE);
+        SharedPreferences.Editor  editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(currentList);
+        editor.putString(FILE_NAME,json);
+        editor.apply();
+    }
+    public void load(int dan,int mesec,int godina){
+        String FILE_NAME=dan+"_"+mesec+"_"+godina;
+        SharedPreferences sharedPreferences = getSharedPreferences(FILE_NAME,MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString(FILE_NAME,null);
+        Type type = new TypeToken<List<Task>>() {}.getType();
+        currentList = gson.fromJson(json,type);
+        if(currentList==null){
+            currentList = new ArrayList<>();
+        }
+
+    }
 
 
 }
